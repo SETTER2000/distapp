9098f811b817987b82eff70438b93235ec5a87d0
SETTER <lphp@mail.ru>
2018-11-28 03:36:36 +0300

    RS

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-//import "github.com/ipfs/go-ipfs"
+import "github.com/llSourcell/kerala"
+
+
+
+func main()  {
+	// Запустить узел
+	node, err := kerala.StartNode()
+	if err != nil{
+		panic(err)
+	}
+
+	// Добавить текст в IPFS (создать MerkleDAG)
+	var userInput = r.Form["sometext"]
+	Key, err := kerala.AddString(node, userInput[0])
+
+	// Получить текст из IPFS (извлечь MerkleDAG)
+	tweetArray, _ := kerala.GetStrings(node)
+}
